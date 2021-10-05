b189d0580deafcdff7fd301b1a5f8e258592e5c3
vv_tomilin <backatom1@gmail.com>
2021-10-05 11:23:39 +0500

    Работа с локальным состоянием <useState>

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 const TodoForm: React.FC = () => {
+    const [title, setTitle] = useState<string>('');
+
+    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.target.value)
+    };
+
+    const keyPressHandler = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter') {
+            console.log(title);
+            setTitle('');
+            
+        }
+    };
+
     return (
         <div className="input-field mt2">
-            <input type="text" id="title" placeholder="Ввведите название дела"/>
+            <input 
+                onChange={changeHandler}
+                onKeyPress={keyPressHandler}
+                value={title}
+                type="text" 
+                id="title" 
+                placeholder="Ввведите название дела"/>
             <label htmlFor="title" className="active">Ввведите название дела</label>
         </div>
     );
